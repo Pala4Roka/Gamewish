8b421e1a4ab41260857dd65d8023e6e97783e980
Pala4Roka <128314775+Pala4Roka@users.noreply.github.com>
2023-05-29 02:34:02 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -21,9 +21,29 @@ var steamButton = document.querySelector('.steam');
 				epicWindow.focus();
 			}
 		});
-		var accountWindow = document.getElementById('accountWindow');
-        var accountMenu = document.getElementById('accountMenu');
-
-        accountWindow.addEventListener('click', function() {
-            accountMenu.classList.toggle('active');
-        });
+	const gameCards = document.querySelectorAll('.game-card');
+    const modal = document.getElementById('modal');
+    const modalImage = modal.querySelector('img');
+    const modalDescription = modal.querySelector('.description');
+    const modalCost = modal.querySelector('.cost');
+    const closeButton = modal.querySelector('.close-button');
+    
+    gameCards.forEach(card => {
+      const readMoreButton = card.querySelector('.read-more-button');
+      
+      readMoreButton.addEventListener('click', () => {
+        const imageSrc = card.querySelector('img').src;
+        const description = card.dataset.description;
+        const cost = card.dataset.cost;
+        
+        modalImage.src = imageSrc;
+        modalDescription.textContent = description;
+        modalCost.textContent = `Cost: ${cost}`;
+        
+        modal.classList.add('open');
+      });
+    });
+    
+    closeButton.addEventListener('click', () => {
+      modal.classList.remove('open');
+    });
